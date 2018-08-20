3b877aa0034ec8caa8cb8de2144d3e88f5e29472
wuping9090 <wuping9090@163.com>
2018-08-20 10:57:41 +0800

    add module lib-base

---
+++ lib-base/src/main/java/com/peyton/base/ui/BaseActivity.java
@@ -0,0 +1,6 @@
+package com.peyton.base.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ lib-base/src/main/java/com/peyton/base/ui/BaseFragment.java
@@ -0,0 +1,6 @@
+package com.peyton.base.ui;
+
+import android.support.v4.app.Fragment;
+
+public class BaseFragment extends Fragment {
+}
